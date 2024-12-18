675de44e8b09b9e7a943931227782641feb14e91
RichardRain <alexander.hell.scorpion@gmail.com>
2024-12-18 22:02:32 +0300

    fix: испралвена утилитарная функция convertToDTO

--- backend/src/utils/dtoConverter.ts
+++ backend/src/utils/dtoConverter.ts
@@ -1,6 +1,5 @@
 export function convertToDTO<T>(dbObject: any): T {
-  // Удаляем поле _id из объекта, так как оно не должно быть в DTO, eslint не пропускает с неиспользуюмщейся переменной
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { _id, ...filteredObject } = dbObject['_doc'];
+  const {...filteredObject } = dbObject['_doc'];
+  delete filteredObject['_id'];
   return filteredObject as T;
 }
